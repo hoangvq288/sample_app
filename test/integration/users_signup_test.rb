f69f564c196eb95a7a83790353dfce886abbee03
Vu Quoc Hoang <hoangvq288@gmail.com>
2016-09-05 14:36:29 +0700

    Chapter 11

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,6 +4,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "cannot create user without valid info" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -15,17 +20,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "Hoang Vu",
-                                         email: "hoangvq@gmail.com",
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
                                          password:              "password",
                                          password_confirmation: "password" } }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    log_in_as(user)
+    assert_not is_logged_in?
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.nil?
     assert is_logged_in?
   end
 end
